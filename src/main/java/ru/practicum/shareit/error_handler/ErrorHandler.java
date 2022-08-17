c8912480e31388015322921bb2fbab99040ddc66
Sudarikov_Iaroslav <yarik846@mail.ru>
2022-08-17 13:26:36 +0300

    refactor: ItemValidationService заменён на аннотации

--- src/main/java/ru/practicum/shareit/error_handler/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error_handler/ErrorHandler.java
@@ -1,24 +1,17 @@
 package ru.practicum.shareit.error_handler;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.item.exception.DeniedAccessException;
-import ru.practicum.shareit.item.exception.InvalidItemException;
 import ru.practicum.shareit.item.exception.OwnerNotFoundException;
 import ru.practicum.shareit.user.exception.EmailConflictException;
-import ru.practicum.shareit.user.exception.InvalidUserException;
 import ru.practicum.shareit.user.exception.UserNotFoundException;
 
 @RestControllerAdvice
 public class ErrorHandler {
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handle(InvalidUserException e) {
-        return new ErrorResponse("Ошибка валидации пользователя", e.getMessage());
-    }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
@@ -33,12 +26,6 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handle(InvalidItemException e) {
-        return new ErrorResponse("Ошибка валидации item", e.getMessage());
-    }
-
-    @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handle(OwnerNotFoundException e) {
         return new ErrorResponse("Не найден владелец вещи", e.getMessage());
@@ -48,5 +35,11 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handle(DeniedAccessException e) {
         return new ErrorResponse("Отказано в доступе", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handle(MethodArgumentNotValidException e) {
+        return new ErrorResponse("Ошибка валидации 400:", e.getMessage());
     }
 }
--- src/main/java/ru/practicum/shareit/item/exception/InvalidItemException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.item.exception;
-
-public class InvalidItemException extends RuntimeException {
-    public InvalidItemException(String message) {
-        super(message);
-    }
-}
